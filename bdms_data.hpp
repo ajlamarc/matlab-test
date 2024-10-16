646e8181b2d3c608c65c670a12d26d1cef1ce695
AJ LaMarc <ajlamarc@gmail.com>
2024-10-16 15:56:14 -0400

    remove const

--- bdms_data.hpp
+++ bdms_data.hpp
@@ -5,10 +5,10 @@ class BDMSDataManager : public BaseBDMSDataManager
 public:
     using BaseBDMSDataManager::BaseBDMSDataManager; // Inherit constructors
 
-    const mxArray *getArray(const SessionID &sessionID, std::vector<std::string> &dataIDs);
+    mxArray *getArray(const SessionID &sessionID, std::vector<std::string> &dataIDs);
 };
 
-const mxArray *BDMSDataManager::getArray(const SessionID &sessionID, std::vector<std::string> &dataIDs)
+mxArray *BDMSDataManager::getArray(const SessionID &sessionID, std::vector<std::string> &dataIDs)
 {
     auto dataFutures = getDataArraysAsync(sessionID, dataIDs);
     std::vector<GenericVector> chunks(dataFutures.size());
@@ -22,7 +22,7 @@ const mxArray *BDMSDataManager::getArray(const SessionID &sessionID, std::vector
 
     // Create MATLAB uint8 column vector with the expected size
     mwSize dims[2] = {totalByteSize, 1};
-    const mxArray *outputBytes = mxCreateNumericArray(2, dims, mxUINT8_CLASS, mxREAL);
+    mxArray *outputBytes = mxCreateNumericArray(2, dims, mxUINT8_CLASS, mxREAL);
     char *outputBuffer = static_cast<char *>(mxGetData(outputBytes));
 
     size_t byteOffset = 0;
